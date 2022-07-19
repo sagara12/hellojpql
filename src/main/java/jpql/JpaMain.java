9660ec8bd602bdd8197718fd1b1ce6bda151eecf
sagara12 <sagara12@naver.com>
2022-07-20 03:59:44 +0900

    2022-07-17 JPQL 기본 문법 2022-07-17 select 프로젝션 2022-07-17 join 2022-07-17 JPQL 사용자 정의 함수 2022-07-20 페치 조인 1

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -13,24 +13,47 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-                Team team = new Team();
-                team.setName("teamA");
-                em.persist(team);
+                Team team1 = new Team();
+                team1.setName("teamA");
+                em.persist(team1);
 
-                Member member = new Member();
-                member.setUsername("member" );
-                member.setAge(10);
+                Team team2 = new Team();
+                team2.setName("teamB");
+                em.persist(team2);
 
-                member.setTeam(team);
+                Member member1 = new Member();
+                member1.setUsername("회원1");
+                member1.setTeam(team1);
+                em.persist(member1);
 
-                em.persist(member);
+                Member member2 = new Member();
+                member2.setUsername("회원2");
+                member2.setTeam(team1);
+                em.persist(member2);
 
-
+                Member member3 = new Member();
+                member3.setUsername("회원3");
+                member3.setTeam(team2);
+                em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query =
+            String query = "select m From Member m join fetch m.team";
+            
+            List<Member> result = em.createQuery(query, Member.class).getResultList();
+
+            for (Member member : result) {
+
+                System.out.println("member.getUsername() = " + member.getUsername() + "," + "member.getTeam().getName() = "+ member.getTeam().getName());
+                //회원 1 팀A (SQL)
+                //회원 2 팀A (영속성)     -->  이런 경우 N+1 문제
+                //회원 3 팀B (SQL)
+            }
+
+            /*//명시적 조인을 해서 별칭을 얻고 별칭을 통해 탐색(실무에서는 묵시적 조인 쓰면 안됨)
+            String query = "select m.username From Team t join t.member m";*/
+/*            String query =
                           "select " +
                                    "case when m.age <= 10 then '학생요금' "+
                                    "     when m.age >= 60 then '경로요금' "+
@@ -41,7 +64,7 @@ public class JpaMain {
             List<String> result = em.createQuery(query, String.class).getResultList();
             for (String s : result) {
                 System.out.println("s = " + s);
-            }
+            }*/
            /* String query = "select m from Member m inner join m.team t";
             List<Member> result = em.createQuery(query, Member.class).getResultList();*/
             /*em.createQuery("select m from Member m order by m.age desc", Member.class)
